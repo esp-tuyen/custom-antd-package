60b29f01c5329c1ce3e06db5f8704fcda823aaba
tuyen <tuyen.tran@eastplayers.io>
2024-02-17 15:34:46 +0700

    init

---
+++ src/Button/Button.tsx
@@ -0,0 +1,10 @@
+import { Button as ButtonAntd } from "antd";
+import React from "react";
+
+import "./Button.css";
+
+const Button: React.FC = () => {
+  return <ButtonAntd />;
+};
+
+export default Button;
